3a6b5318f7170f47fbf48b2bf27f8444c9b77424
Baozi Wu <baojjeu@gmail.com>
2016-11-24 15:46:22 +0800

    Add test for DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -66,7 +66,7 @@ app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
-    res.status(404).send();
+    res.status(400).send();
   }
 
   Todo.findByIdAndRemove(id).then((todo) => {
@@ -74,10 +74,10 @@ app.delete('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
 
-    res.status(200).send(todo);
+    res.send({ todo });
   }).catch((e) => {
     res.status(400).send();
-  })
+  });
 });
 
 app.listen(port, () => {
